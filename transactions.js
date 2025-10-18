90b61e90b26feaa1f56ba41632db2b6710a44b01
uma shankar <umashankar.kousha@gmail.com>
2025-10-18 19:40:07 +0530

    Style and Arrow function Modified

--- transactions.js
+++ transactions.js
@@ -20,18 +20,22 @@ function updateTransactions(data) {
   transactionsContainerEl.innerHTML = "";
   if (data.length !== 0) {
     data.forEach((element) => {
+      let typeClass = "expense";
+      let amountClass = "red";
+      let sign = "-";
+      if (element.type === "Income") {
+        typeClass = "income";
+        amountClass = "green";
+        sign = "+";
+      }
       transactionsContainerEl.innerHTML += `<li class="transaction-item">
             <div class="transaction-item-note">
            
-              <span class="type ${
-                element.type === "Income" ? "income" : "expense"
-              }">${element.type}</span>
+              <span class="type ${typeClass}">${element.type}</span>
               <span class="name">${element.name}</span>
             </div>
             
-             <p class="amount ${element.type === "Income" ? "green" : "red"}">${
-        element.type === "Income" ? "+" : "-"
-      } ${element.amount} </p>   
+             <p class="amount ${amountClass}">${sign} ${element.amount} </p>   
         </li>`;
     });
   }
@@ -43,10 +47,12 @@ filterEl.addEventListener("change", () => {
   let filteredData;
   if (filterEl.value === "All") {
     filteredData = data;
+    transactionNameEl.textContent = "All Transactions";
   } else {
     filteredData = data.filter((element) => {
       return filterEl.value === element.type;
     });
+    transactionNameEl.textContent = `${filterEl.value} Transactions`;
   }
   updateTransactions(filteredData);
 });
